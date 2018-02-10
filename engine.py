a7976f1d3f1c63a7eac0f64c47aab9d7aa8cbdfc
mknittel <mknittel@g.hmc.edu>
2018-02-10 15:02:57 -0800

    Added neighborhoods and parser to engine

--- engine.py
+++ engine.py
@@ -1,15 +1,32 @@
 #Engine
 import random
+import argparse
 
-numAgents = 100
+NUM_AGENTS = 100
+NUM_WEEKS = 100
+
+parser = argparse.ArgumentParser(description="process graph and station function")
+parser.add_argument('graph', metavar='GRAPH', type=str, help='Path to graph file')
+parser.add_argument('place_func', metavar='PLACEFUNC', type=str, help='Path to placement function file')
+args = parser.parse_args
+
+graph = Graph(args[graph])
+neighborhoods = {}
+num_nodes = len(graph.nodes)
+
+for node in graph:
+    neighborhood[node] = {}
 
 #create agents
 agentList = []
-for i in range(numAgents):
-	agent = Agent()
+for i in range(NUM_AGENTS):
 	#set home to be current node
 	#assign random nodes work
 	schedule = []
+
+    home_index = random.range(0, num_nodes)
+    home = graph.nodes[home_index]
+
 	for j in range(5):
 		#start at home
 		#go to work
@@ -25,3 +42,13 @@ for i in range(numAgents):
 			pass
 			#create random destinations in graph
 		#return home
+
+    agent = Agent(schedule)
+    neighborhood[home].append(agent)
+
+# Turn neighborhood into Neighborhood data structure
+for node in neighborhoods.keys():
+    agents = neighborhoods[node]
+    neighborhood = Neighborhood(agents)
+    neighborhoods[node] = neighborhood
+
